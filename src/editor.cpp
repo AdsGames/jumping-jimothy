14fa5d8bf29e3a3ea184206a6dd478cebd99cae3
Allan Legemaate <alegemaate@gmail.com>
2017-05-06 12:29:51 -0400

    Tilesss

--- src/editor.cpp
+++ src/editor.cpp
@@ -47,15 +47,15 @@ void editor::update(){
 void editor::draw(){
   // Background
   al_clear_to_color( al_map_rgb(200,200,255));
-x_str
+
   // Grid
   for( int i = 0; i < 1024; i += 32){
-    al_draw_line( i, 0, i, 768, al_map_rgb( 0, 0, 0), 1);
+    //al_draw_line( i, 0, i, 768, al_map_rgb( 0, 0, 0), 1);
   }
 
   for( int i = 0; i < 768; i += 32){
-    al_draw_line( 0, i, 1024, i, al_map_rgb( 0, 0, 0), 1);
-  }srand(time(NULL));
+    //al_draw_line( 0, i, 1024, i, al_map_rgb( 0, 0, 0), 1);
+  }
 
   // Boxes
   for( unsigned int i = 0; i < editorBoxes.size(); i ++){
