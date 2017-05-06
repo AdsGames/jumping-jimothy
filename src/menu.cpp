cb5724a590c5293cbfe5a2fb77e8c041047e197b
Allan Legemaate <alegemaate@gmail.com>
2017-05-06 15:27:32 -0400

    Menu and main

--- src/menu.cpp
+++ src/menu.cpp
@@ -2,10 +2,13 @@
 
 menu::menu(){
   // Title image
-  ALLEGRO_BITMAP title = tools::load_bitmap_ex("images/title.png");
+  ALLEGRO_BITMAP *title = tools::load_bitmap_ex("images/title.png");
 
-  title_images[170]
-
+  for( int i = 0; i < 10; i++){
+    for( int t = 0; t < 17; t++){
+      title_images[i + t*17] = al_create_sub_bitmap( title, i * 256, t * 256, 256, 256);
+    }
+  }
 }
 
 menu::~menu(){
@@ -16,5 +19,7 @@ void menu::update(){
 }
 
 void menu::draw(){
-
+  for( int i = 0; i < 17; i++){
+    al_draw_bitmap( title_images[i], 0, 0, 0);
+  }
 }
