b67af0c1a04c378d40d8419b06150e65059f223f
Allan Legemaate <alegemaate@gmail.com>
2017-05-06 15:08:40 -0400

    I did stuff

--- include/menu.h
+++ include/menu.h
@@ -1,7 +1,21 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#include <vector>
+#include <string>
+
+#include "mouseListener.h"
+#include "keyListener.h"
+#include "tools.h"
+
 #include "state.h"
+
 
 // Menu
 class menu : public state{
@@ -13,6 +27,7 @@ class menu : public state{
     void draw();
 
   protected:
+    ALLEGRO_BITMAP *title_images[170];
 
   private:
 };
