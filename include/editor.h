ad5ddacb6283644b1f27697e107828aa7916d9eb
Allan Legemaate <alegemaate@gmail.com>
2017-05-06 08:34:53 -0400

    Editor mode is a lil better

--- include/editor.h
+++ include/editor.h
@@ -5,7 +5,18 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
 
+#include <vector>
+
+#include "mouseListener.h"
+#include "tools.h"
+
 #include "state.h"
+
+// Nice editor boxes
+struct editor_box{
+  int x;
+  int y;
+};
 
 class editor : public state{
   public:
@@ -16,7 +27,9 @@ class editor : public state{
     void update();
     void draw();
   protected:
+    ALLEGRO_BITMAP *image_box;
 
+    std::vector<editor_box> editorBoxes;
   private:
 };
 
