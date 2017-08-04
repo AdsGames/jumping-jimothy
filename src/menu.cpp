251b7847838bc7cfb559028d5a12cd77926b8acf
Danny <danwardvs@adsgames.net>
2017-08-03 21:41:12 -0400

    "nice" main menu gui

--- src/menu.cpp
+++ src/menu.cpp
@@ -6,19 +6,40 @@ menu::menu(){
   menu_font = al_load_ttf_font( "fonts/fantasque.ttf", 18, 0);
 
 
-  menu_buttons[menu_button_edit] = button( 200, 500, 100,100, "Level Editor",menu_font);
-  menu_buttons[menu_button_exit] = button( 0, 700, 50,68, "Exit",menu_font);
-  menu_buttons[menu_button_play] = button( 400, 310, 100,50, "Play",menu_font);
-  menu_buttons[menu_button_help] = button( 200, 210, 75,50, "Halp plz v2",menu_font);
+  menu_buttons[menu_button_edit] = button( 320, 630, 180,90, "Level Editor",menu_font);
+  menu_buttons[menu_button_edit].setVisibility(false);
+
+  menu_buttons[menu_button_exit] = button( 750, 630, 180,90, "Exit",menu_font);
+  menu_buttons[menu_button_exit].setVisibility(false);
+
+
+  menu_buttons[menu_button_play] = button( 40, 590, 250,140, "Play",menu_font);
+  menu_buttons[menu_button_play].setVisibility(false);
+
+  menu_buttons[menu_button_help] = button( 540, 630, 180,90, "Halp plz v2",menu_font);
+  menu_buttons[menu_button_help].setVisibility(false);
+
 
 
   // Title image
-  title = tools::load_bitmap_ex("images/title.png");
+
+  button_edit = tools::load_bitmap_ex("images/button_edit.png");
+  button_help = tools::load_bitmap_ex("images/button_help.png");
+  button_exit = tools::load_bitmap_ex("images/button_exit.png");
+
+
+
+
+  title = tools::load_bitmap_ex("images/title_static.png");
+  playbutton_frame = tools::load_bitmap_ex("images/playbutton_frame.png");
+  playbutton_frame_hover = tools::load_bitmap_ex("images/playbutton_frame_hover.png");
+
+
 
   // Slice up title images
-  for( int i = 0; i < 10; i++)
-    for( int t = 0; t < 17; t++)
-      title_images[i + t * 10] = al_create_sub_bitmap( title, i * 256, t * 256, 256, 256);
+ // for( int i = 0; i < 10; i++)
+  //  for( int t = 0; t < 17; t++)
+  //    title_images[i + t * 10] = al_create_sub_bitmap( title, i * 256, t * 256, 256, 256);
 
   // Load play button
   play = tools::load_bitmap_ex("images/playbutton.png");
@@ -41,11 +62,11 @@ menu::~menu(){
   for( int i = 0; i < 50; i++)
     al_destroy_bitmap( play_images[i]);
 
-  for( int i = 0; i < 10; i++)
-    for( int t = 0; t < 17; t++)
-      al_destroy_bitmap( title_images[i + t * 10]);
+  //for( int i = 0; i < 10; i++)
+    //for( int t = 0; t < 17; t++)
+      //al_destroy_bitmap( title_images[i + t * 10]);
 
-  al_destroy_bitmap( prompt_image);
+  //al_destroy_bitmap( prompt_image);
   al_destroy_bitmap( play);
   al_destroy_bitmap( title);
 }
@@ -82,25 +103,60 @@ void menu::update(){
   counter_prompt = (counter_play >= 50) ? !counter_prompt : counter_prompt;
   counter_play = (counter_play >= 50) ? 0 : counter_play;
 
-  // Go to game
-  if( keyListener::anyKeyPressed || joystickListener::anyButtonPressed)
-    set_next_state(STATE_GAME);
+  // Click anywhere
+  //if( keyListener::anyKeyPressed || joystickListener::anyButtonPressed)
+  //   set_next_state(STATE_GAME);
 }
 
 // Draw images to screen
 void menu::draw(){
 
   // Background
-  al_clear_to_color( al_map_rgb(200,200,255));
+  al_clear_to_color( al_map_rgb(50,50,50));
 
   // Title
-  al_draw_scaled_bitmap( title_images[counter_title], 0, 0, 256, 256, 0, 0, 1024, 768, 0);
+  al_draw_scaled_bitmap( title, 0,0, 175, 160, 50, 50, 612,560, 0);
+  //al_draw_bitmap(title,300,300,0);
+  //al_draw_bitmap(playbutton_frame,297,562,0);
 
-  // Play prompt
-  al_draw_scaled_bitmap( play_images[counter_play], 0, 0, 64, 32, 300, 565, 256, 128, 0);
+  int playbutton_x = 50;
+  int playbutton_y = 605;
 
-  if( counter_prompt)
-    al_draw_scaled_bitmap( prompt_image, 0, 0, 128, 16, 290, 710, 256, 32, 0);
+
+  if(menu_buttons[menu_button_play].hover())
+    al_draw_scaled_bitmap( playbutton_frame_hover, 0, 0, 70, 38,playbutton_x-12, playbutton_y-12, 280, 152, 0);
+  else
+    al_draw_scaled_bitmap( playbutton_frame, 0, 0, 70, 38,playbutton_x-12, playbutton_y-12, 280, 152, 0);
+
+  al_draw_scaled_bitmap( play_images[counter_play], 0, 0, 64, 32,playbutton_x, playbutton_y, 256, 128, 0);
+
+  if(menu_buttons[menu_button_edit].hover())
+    al_draw_scaled_bitmap( playbutton_frame_hover, 0, 0, 70, 38,320, 630, 70*3, 38*3, 0);
+  else
+    al_draw_scaled_bitmap( playbutton_frame, 0, 0, 70, 38,320, 630, 70*3, 38*3, 0);
+
+  al_draw_scaled_bitmap(button_edit, 0, 0, 70, 38,320, 630, 70*3, 38*3, 0);
+
+
+  if(menu_buttons[menu_button_help].hover())
+    al_draw_scaled_bitmap( playbutton_frame_hover, 0, 0, 70, 38,530, 630, 70*3, 38*3, 0);
+  else
+    al_draw_scaled_bitmap( playbutton_frame, 0, 0, 70, 38,530, 630, 70*3, 38*3, 0);
+
+    al_draw_scaled_bitmap( button_help, 0, 0, 70, 38,530, 630, 70*3, 38*3, 0);
+
+
+
+  if(menu_buttons[menu_button_exit].hover())
+    al_draw_scaled_bitmap( playbutton_frame_hover, 0, 0, 70, 38,740, 630, 70*3, 38*3, 0);
+  else
+    al_draw_scaled_bitmap( playbutton_frame, 0, 0, 70, 38,740, 630, 70*3, 38*3, 0);
+
+  al_draw_scaled_bitmap( button_exit, 0, 0, 70, 38,740, 630, 70*3, 38*3, 0);
+
+
+  //if( counter_prompt)
+   // al_draw_scaled_bitmap( prompt_image, 0, 0, 128, 16, 290, 710, 256, 32, 0);
 
    // Update buttons
   for( int i = 0; i < BUTTON_COUNT; i++){
