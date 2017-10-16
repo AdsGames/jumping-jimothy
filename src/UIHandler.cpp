7185420235d2e82ad03d5434e1d091b5b9b7856b
Allan Legemaate <alegemaate@gmail.com>
2017-10-16 19:54:30 -0400

    Removied some pointies
    -No longer returning pointer from getElementByText becaues it is unsafe

--- src/UIHandler.cpp
+++ src/UIHandler.cpp
@@ -18,27 +18,27 @@ void UIHandler::createButton(int newX, int newY,std::string newText,ALLEGRO_FONT
 }
 
 void UIHandler::createAnchoredButton(std::string newText,ALLEGRO_FONT *newFont,std::string newAnchor, bool newJustification){
-  Button *newButton = new Button(0,getElementByText(newAnchor) -> getY(),newText,newFont);
+  Button *newButton = new Button(0,getElementByText(newAnchor).getY(),newText,newFont);
 
   if(newJustification)
-    newButton -> setX(getElementByText(newAnchor) -> getRightX());
+    newButton -> setX(getElementByText(newAnchor).getRightX());
   else
-    newButton -> setX(getElementByText(newAnchor) -> getRightX());
+    newButton -> setX(getElementByText(newAnchor).getRightX());
 
   ui_elements.push_back(newButton);
 }
 
-UIElement* UIHandler::getElementByText(std::string newText){
+UIElement UIHandler::getElementByText(std::string newText){
   for( unsigned int i = 0; i < ui_elements.size();i++)
     if( ui_elements.at(i) -> getText() == newText)
-      return ui_elements.at(i);
+      return *ui_elements.at(i);
 
   // This will probably crash if the element is not found
   // So be careful
   // Plz dont hurt me
   //  Edit: I do give ample warning before the tortured aftereffects
   //  Just watch the console
-  UIElement* failure = new UIElement();
+  UIElement failure;
   std::cout << "WARNING: Failed to find UI element " << newText << ", may(hint:will) be unpredictable\n";
   return failure;
 }
