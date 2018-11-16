06230888796c432e2e5eccd3c7163f4110c04d49
Allan Legemaate <alegemaate@gmail.com>
2018-11-16 00:25:46 -0500

    Fixed character speed and re-removed depend and layout

--- src/Character.cpp
+++ src/Character.cpp
@@ -120,7 +120,8 @@ void Character::update(){
   angle = body -> GetAngle();
   double yVel = getBody() -> GetLinearVelocity().y;
 
-  float x_velocity_ground = 10;
+  #warning this is too slow!
+  float x_velocity_ground = 4;
   float x_velocity_air = 0.8;
   float x_velocity_air_max=4;
 
