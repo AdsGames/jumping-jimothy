aa35268e3edc16957ecafbda9267f64171ca2642
Allan Legemaate <alegemaate@gmail.com>
2018-11-26 22:48:31 -0500

    Added more unit tests for tools

--- test/include/util/ToolsUnitTest.h
+++ test/include/util/ToolsUnitTest.h
@@ -14,9 +14,11 @@ class ToolsUnitTest : public Test::Suite {
     ToolsUnitTest() {
       TEST_ADD(ToolsUnitTest::stringToInt_ut)
       TEST_ADD(ToolsUnitTest::stringToFloat_ut)
+      TEST_ADD(ToolsUnitTest::toString_ut)
     }
 
   private:
     void stringToInt_ut();
     void stringToFloat_ut();
+    void toString_ut();
 };
